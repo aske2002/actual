52c458605191255c2324bcdb293db5fcbec75902
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2026-02-23 19:50:58 +0000

    Update useSyncedPrefs

--- packages/desktop-client/src/hooks/useSyncedPrefs.ts
+++ packages/desktop-client/src/hooks/useSyncedPrefs.ts
@@ -8,11 +8,8 @@ type SetSyncedPrefsAction = (value: Partial<SyncedPrefs>) => void;
 
 /** @deprecated: please use `useSyncedPref` (singular) */
 export function useSyncedPrefs(): [SyncedPrefs, SetSyncedPrefsAction] {
-  const saveSyncedPrefsMutation = useSaveSyncedPrefsMutation();
-  const saveSyncedPrefs: SetSyncedPrefsAction = newValue => {
-    saveSyncedPrefsMutation.mutate(newValue);
-  };
-  const syncedPrefsQuery = useQuery(prefQueries.listSynced());
+  const { mutate: saveSyncedPrefs } = useSaveSyncedPrefsMutation();
+  const { data: syncedPrefs } = useQuery(prefQueries.listSynced());
 
-  return [syncedPrefsQuery.data as SyncedPrefs, saveSyncedPrefs];
+  return [syncedPrefs as SyncedPrefs, saveSyncedPrefs];
 }
